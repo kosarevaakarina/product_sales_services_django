17e5b11b789ac49b022ac8babe6fce0033c1e739
user <kosareva.karina2013@yandex.ru>
2023-05-08 14:43:30 +0300

    change admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,17 +5,18 @@ from catalog.models import Category, Product, Contact
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category',)
+    list_display = ('name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description')
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'phone')
+    list_display = ('name', 'phone')
+    list_filter = ('name',)
     search_fields = ('name', 'phone')
