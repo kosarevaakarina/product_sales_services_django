09cf8f8865e83c5b1cb93a0ac7292f29baeda9aa
user <kosareva.karina2013@yandex.ru>
2023-05-10 01:25:36 +0300

    change catalog/

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, catalog, product_items
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-     path('', home),
-     path('contacts/', contacts),
-    ]
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('catalog/', catalog, name='catalog'),
+    path('product_items/<int:pk>/', product_items, name='product_items')
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 
-from catalog.models import Product, Contact
+from catalog.models import Product, Contact, Category
 
 
 def home(request):
     last_products = Product.objects.order_by('-created_date')[:5]
     for product in last_products:
         print(product.name)
-    return render(request, 'catalog/home.html')
+    context = {
+        "category_list": Category.objects.all(),
+        "product_list": Product.objects.all()[:3],
+        "title": "Товары месяца"
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def catalog(request):
+    context = {
+        "category_list": Category.objects.all(),
+        "product_list": Product.objects.all(),
+        "title": "Каталог товаров"
+    }
+    return render(request, 'catalog/catalog.html', context)
+
+
+def product_items(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'product': product_item,
+        'title': product_item.name
+    }
+    return render(request, 'catalog/product_items.html', context)
 
 
 def contacts(request):
@@ -15,6 +38,9 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        Contact(name=name, phone=phone, message=message)
+        Contact(name=name, phone=phone, message=message).save()
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+    context = {
+        "title": "Обратная связь"
+    }
+    return render(request, 'catalog/contacts.html', context)
