31c609cef5425384fa919385dff57731df765770
user <kosareva.karina2013@yandex.ru>
2023-05-15 22:37:46 +0300

    change config/urls and config/setting

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'catalog'
+    # local apps
+    'catalog',
+    'blog'
 ]
 
 MIDDLEWARE = [
