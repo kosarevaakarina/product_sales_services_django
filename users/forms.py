6a5d404db9c4c197642d98403c08eda82c174568
user <kosareva.karina2013@yandex.ru>
2023-06-13 17:56:18 +0300

    change forms

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django import forms
+
+from catalog.forms import FormStyleMixin
 from users.models import User
 
 
-class UserForm(UserChangeForm):
+class UserForm(FormStyleMixin, UserChangeForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'avatar')
@@ -14,7 +16,7 @@ class UserForm(UserChangeForm):
         self.fields['password'].widget = forms.HiddenInput()
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(FormStyleMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
